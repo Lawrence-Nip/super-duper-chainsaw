1dd48c732255b3818fd6fbd074d5c538177158a0
Lawrence Nip <lawrence.nip@outlook.com>
2021-05-05 04:10:43 +0100

    fixed typo, removed unnecessary import

--- linear_regression_example.py
+++ linear_regression_example.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
 import matplotlib.pyplot as pyplot
 from matplotlib import style
 import pickle
@@ -15,22 +14,23 @@ x = np.array(data.drop((predict), 1))
 y = np.array(data[predict])
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-# TRAIN AND TEST BEST MODEL IN 30 TRIES
-# best = 0
-# for _ in range(30):    
-#     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+# TRAIN AND TEST BEST MODEL IN 30 TRIES (comment out after desired model is found)
+best = 0
+for _ in range(30):    
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-#     linear = linear_model.LinearRegression()
-#     linear.fit(x_train, y_train)
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
 
-#     acc = linear.score(x_test, y_test)
-#     print(acc)
+    acc = linear.score(x_test, y_test)
+    print(acc)
 
-#     if acc > best:
-#         best = acc
-#         with open("studentmodel.pickle", "wb") as f:
-#             pickle.dump(linear, f)
-# print("best = ", best)
+    if acc > best:
+        best = acc
+        with open("linear_model.pickle", "wb") as f:
+            pickle.dump(linear, f)
+print("best = ", best)
+# TRAIN AND TEST BEST MODEL IN 30 TRIES (comment out after desired model is found)
 
 
 pickle_in = open("linear_model.pickle", "rb")
